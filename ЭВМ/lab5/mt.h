99e8895d2c17213765e3d1fca6c2711ea389e1d8
Muriam <d_muriam@inbox.ru>
2017-11-02 23:24:54 +0700

    del

--- ЭВМ/lab5/mt.h
+++
@@ -1,6 +0,0 @@
-enum colors{BLACK,RED,GREEN,YELLOW,BLUE,VIOLET,BIRUZ,WHITE};
-int mt_clrscr(void);
-int mt_gotoXY(int x, int y);
-int mt_getscreensize(int *rows, int *cols);
-int mt_setfgcolor(enum colors color);
-int mt_setbgcolor(enum colors color);
--- ЭВМ/lab6/lab5.h
+++
@@ -1,7 +0,0 @@
-
-struct termios term;
-enum keys {undefined=0, l=1, s=2, r=3, t=4, i=5, f5=6, f6=7, down=8, up=9, k_left=10, k_right=11, enter=12, esc=13 };
-int rk_readkey(enum keys *k);
-int rk_mytermsave(void);
-int rk_mytermregime(int regime,int vtime,int vmin,int echo,int sigint);
-
